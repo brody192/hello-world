b266bac12cda2805ea67e54e240958f259eb96f3
Brody Over <brodyover@gmail.com>
2023-09-20 23:39:57 -0400

    support multiple ports

--- go.mod
+++ go.mod
@@ -1,13 +1,13 @@
 module main
 
-go 1.20
+go 1.21
+
+toolchain go1.21.1
 
 require (
-	github.com/brody192/basiclogger v0.1.2
-	github.com/brody192/ext v0.1.13
-	github.com/go-chi/chi/v5 v5.0.8
+	github.com/brody192/ext v0.1.15
+	github.com/brody192/logger v0.0.1
+	github.com/go-chi/chi/v5 v5.0.10
 	github.com/go-chi/cors v1.2.1
-	github.com/go-chi/httprate v0.7.4
+	golang.org/x/exp v0.0.0-20230905200255-921286631fa9
 )
-
-require github.com/cespare/xxhash/v2 v2.2.0 // indirect
--- main.go
+++ main.go
@@ -1,19 +1,23 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"net"
 	"net/http"
 	"time"
 
-	"github.com/brody192/basiclogger"
 	"github.com/brody192/ext/exthandler"
 	"github.com/brody192/ext/extmiddleware"
 	"github.com/brody192/ext/extrespond"
-	"github.com/brody192/ext/extutil"
+	"github.com/brody192/logger"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/cors"
-	"github.com/go-chi/httprate"
+	"golang.org/x/exp/slog"
 )
+
+type portKey struct{}
 
 func main() {
 	var r = chi.NewRouter()
@@ -24,7 +28,6 @@ func main() {
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 	r.Use(extmiddleware.LimitBytes(200))
-	r.Use(httprate.LimitByIP(10, 15*time.Second))
 	r.Use(middleware.NoCache)
 	r.Use(cors.AllowAll().Handler)
 
@@ -34,20 +37,47 @@ func main() {
 				return
 			}
 
-			extrespond.PlainText(w, "Hello, World!", http.StatusOK)
+			port, ok := r.Context().Value(portKey{}).(string)
+			if !ok {
+				extrespond.PlainText(w, "no port found for incoming request", http.StatusInternalServerError)
+				return
+			}
+
+			greeting := fmt.Sprintf("Hello, World! - Port %s", port)
+
+			extrespond.PlainText(w, greeting, http.StatusOK)
 		},
 	)
 
-	var port = extutil.EnvPortOr("3001")
+	ports := []string{"8000", "8001", "8002", "8003", "8004", "8005"}
 
-	var s = &http.Server{
-		Addr:              port,
-		Handler:           r,
-		ReadTimeout:       1 * time.Minute,
-		WriteTimeout:      1 * time.Minute,
-		ReadHeaderTimeout: 1 * time.Second,
+	errChan := make(chan error, 1)
+
+	for _, port := range ports {
+		server := &http.Server{
+			Addr:    ":" + port,
+			Handler: r,
+			ConnContext: func(ctx context.Context, c net.Conn) context.Context {
+				_, port, err := net.SplitHostPort(c.LocalAddr().String())
+				if err != nil {
+					return ctx
+				}
+
+				return context.WithValue(ctx, portKey{}, port)
+
+			},
+			ReadTimeout:       1 * time.Minute,
+			WriteTimeout:      1 * time.Minute,
+			ReadHeaderTimeout: 1 * time.Second,
+		}
+
+		go func(port string) {
+			logger.Stdout.Info("starting server", slog.String("port", port))
+			errChan <- server.ListenAndServe()
+		}(port)
 	}
 
-	basiclogger.InfoBasic.Println("starting server on port " + port[1:])
-	basiclogger.Error.Fatalln(s.ListenAndServe())
+	if err := <-errChan; err != nil {
+		logger.Stderr.Error("server exited", logger.ErrAttr(err))
+	}
 }
